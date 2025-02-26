18b0dccb768f70a3a27956b2df2e3d90bca0cd02
JulianNF <21272816+JulianNF@users.noreply.github.com>
2025-02-26 17:50:10 +0100

    - Fixed incorrect storing of lat/lon values so that they now match correct geoJSON format of [lon,lat]

--- index.js
+++ index.js
@@ -7,44 +7,45 @@ const outputFile = "output/outputfilenamehere.geojson"; // geoJSON file
 const tileSizeDegrees = 0.25; // Width & height. Tile will be centered at coordinate of each point in input data
 const boundaries = { // Tiles will be clipped if they extend outside this area
     lat: {
-        min: -180,
-        max: 180
-    },
-    lon: {
         min: -90,
         max: 90
+    },
+    lon: {
+        min: -180,
+        max: 180
     }
 }
 
-function createSquareTile([xTile, yTile]) {
-    let cornerX = xTile - tileSizeDegrees / 2;
-    let cornerY = yTile - tileSizeDegrees / 2;
+function createSquareTile([longitude, latitude]) {
+    // Define bottom-left corner of tile:
+    let cornerLon = longitude - tileSizeDegrees / 2;
+    let cornerLat = latitude - tileSizeDegrees / 2;
     let tileWidth = tileSizeDegrees;
     let tileHeight = tileSizeDegrees;
 
-    if (cornerX < boundaries.lon.min) {
-        tileWidth = tileSizeDegrees - (cornerX - boundaries.lon.min);
-        cornerX = boundaries.lon.min;
+    if (cornerLon < boundaries.lon.min) {
+        tileWidth = tileSizeDegrees - (cornerLon - boundaries.lon.min);
+        cornerLon = boundaries.lon.min;
     }
-    if (cornerX + tileWidth > boundaries.lon.max) {
-        tileWidth = tileWidth - (cornerX + tileWidth - boundaries.lon.max);
+    if (cornerLon + tileWidth > boundaries.lon.max) {
+        tileWidth = tileWidth - (cornerLon + tileWidth - boundaries.lon.max);
     }
-    if (cornerY < boundaries.lat.min) {
-        tileHeight = tileSizeDegrees - (cornerY - boundaries.lat.min);
-        cornerY = boundaries.lat.min;
+    if (cornerLat < boundaries.lat.min) {
+        tileHeight = tileSizeDegrees - (cornerLat - boundaries.lat.min);
+        cornerLat = boundaries.lat.min;
     }
-    if (cornerY + tileHeight > boundaries.lat.max) {
-        tileHeight = tileHeight - (cornerY + tileHeight - boundaries.lat.max);
+    if (cornerLat + tileHeight > boundaries.lat.max) {
+        tileHeight = tileHeight - (cornerLat + tileHeight - boundaries.lat.max);
     }
 
     return {
         type: "Polygon",
         coordinates: [[
-            [cornerX, cornerY],
-            [cornerX + tileWidth, cornerY],
-            [cornerX + tileWidth, cornerY + tileHeight],
-            [cornerX, cornerY + tileHeight],
-            [cornerX, cornerY]
+            [cornerLon, cornerLat],
+            [cornerLon + tileWidth, cornerLat],
+            [cornerLon + tileWidth, cornerLat + tileHeight],
+            [cornerLon, cornerLat + tileHeight],
+            [cornerLon, cornerLat]
         ]]
     }
 };
